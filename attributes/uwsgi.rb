18a402b2b68c3387ac731a3415a795d86e73f326
Eugene Alekseev <elekseev@clodo.ru>
2014-06-26 09:38:53 +0400

    Add uwsgi umask attribute to set umask (useful for uwsgi socket creation). Add setting of different uwsgi socket types creation (uwsgi, http, fastcgi etc).

--- attributes/uwsgi.rb
+++ attributes/uwsgi.rb
@@ -1,5 +1,7 @@
 default['graphite']['uwsgi']['socket'] = '/tmp/uwsgi.sock'
+default['graphite']['uwsgi']['socket_type'] = ''
 default['graphite']['uwsgi']['workers'] = 8
 default['graphite']['uwsgi']['carbon'] = '127.0.0.1:2003'
 default['graphite']['uwsgi']['listen_http'] = false
 default['graphite']['uwsgi']['service_type'] = 'runit'
+default['graphite']['uwsgi']['umask'] = '0007'
