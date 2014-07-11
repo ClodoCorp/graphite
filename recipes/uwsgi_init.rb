f2794fdcbc497ec6f778e74647480244b79c317a
Eugene Alekseev <ealekseev@clodo.ru>
2014-07-11 13:25:29 +0400

    Fix bugs in graphite-web init script.

--- recipes/uwsgi_init.rb
+++ recipes/uwsgi_init.rb
@@ -1,3 +1,9 @@
+directory '/var/log/graphite-web' do
+  mode 00700
+  owner node['graphite']['user_account']
+  group node['graphite']['group_account']
+end 
+
 template '/etc/init.d/graphite-web' do
   source 'uwsgi.init.erb'
   mode 00755
